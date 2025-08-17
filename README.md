7564b428dac04587650eb836f45a3fb7178f7e6b
aquaseals <itzme.haya@gmail.com>
2025-08-16 21:42:29 -0400

    basic level + idle animation

--- README.md
+++ README.md
@@ -1,10 +1,4 @@
 # heat-wave
 
-test test test heat wave is a game for athena and for the ith eitch.tio chili code gae jam
 
-hackatime are you working..?? erm hwo do i see if its working..
-
-harro
-testing
- testing
-testinggggg
+wtf am i doing wrong T-T
---
+++ player.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+
+const SPEED = 200.0
+const JUMP_VELOCITY = -300.0
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		$AnimationPlayer.stop()
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
+		$AnimationPlayer.stop() #stops all animations (you can add a left/right walking anim if you want :D )
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		$AnimationPlayer.play('idle') # plays 'idle' animation
+
+	move_and_slide()
